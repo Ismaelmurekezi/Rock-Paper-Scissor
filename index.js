136006ba396ebb8a0f2debc75e99a8b50116c04d
MUREKEZI Ismael <ismaelmurekezi1@gmail.com>
2023-07-07 17:23:24 +0200

    Creation of index  html file and index  javascript file which contain the code for the game

---
+++ index.js
@@ -0,0 +1,69 @@
+const getComputerChoice=()=> {
+    let compChoice = ["Rock", "Paper", "Scissor"];
+    let compRandomChoice = Math.floor(Math.random() * 3);
+    return compChoice[compRandomChoice];
+  }
+  
+  const playRound=(playerSelection, computerSelection)=> {
+    const player = playerSelection.toLowerCase();
+    const computer = computerSelection.toLowerCase();
+  
+    if (
+      (player === "rock" && computer === "paper") ||
+      (player === "paper" && computer === "scissor") ||
+      (player === "scissor" && computer === "rock")
+    ) {
+      return {
+        result: "lose",
+        message: `You lose! ${playerSelection} loses to ${computerSelection}`
+      };
+    } else if (
+      (player === "rock" && computer === "scissor") ||
+      (player === "paper" && computer === "rock") ||
+      (player === "scissor" && computer === "paper")
+    ) {
+      return {
+        result: "win",
+        message: `You win! ${playerSelection} wins against ${computerSelection}`
+      };
+    } else {
+      return {
+        result: "draw",
+        message: `It's a draw!`
+      };
+    }
+  }
+  
+  function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+  
+    for (let round = 1; round <= 5; round++) {
+      const playerSelection = prompt(`Round ${round}: Enter your choice among Rock, Paper, or Scissor`);
+      const computerSelection = getComputerChoice();
+      const roundResult = playRound(playerSelection, computerSelection);
+  
+      console.log(roundResult.message);
+  
+      if (roundResult.result === "win") {
+        playerScore++;
+      } else if (roundResult.result === "lose") {
+        computerScore++;
+      }
+    }
+  
+    console.log(`\nGame Over!\nPlayer Score: ${playerScore}\nComputer Score: ${computerScore}`);
+  
+    // if (playerScore > computerScore) {
+    //   console.log("Congratulations! You win the game!");
+    // } else if (playerScore < computerScore) {
+    //   console.log("Sorry, you lose the game.");
+    // } else {
+    //   console.log("The game ends in a draw.");
+    // }
+    (playerScore> computerScore)?   console.log("Congratulations! You win the game!"):
+    (playerScore < computerScore)?   console.log("Sorry, you lose the game."):
+    console.log("The game ends in a draw.")
+  }
+  
+  game();  
